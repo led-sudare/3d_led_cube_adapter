f03e47b3f37f8161b6fff8902ca0157f2e644d8a
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2019-07-23 21:52:41 +0900

    supports multi resolution data

--- lib/cube_converter.go
+++ lib/cube_converter.go
@@ -26,58 +26,74 @@ func rgb565to888(c565 uint32) []byte {
 	return []byte{r, g, b}
 }
 
-type ledCubeConverter struct {
-	width, height, depth, datalen int
+const rgb565plane = 2
+
+type cubeConverterImpl struct {
+	Width, Height, Depth, DataLen int
 }
 
-var ledConverter8192 CubeConverter
-var ledConverter22500 CubeConverter
+func newLedCubeConverter(width, height, depth int) CubeConverter {
+	return &cubeConverterImpl{
+		Width:   width,
+		Height:  height,
+		Depth:   depth,
+		DataLen: width * height * depth * rgb565plane}
+}
+
+var ledConverter16x32x8 CubeConverter
+var ledConverter15x50x15 CubeConverter
+var ledConverter30x100x30 CubeConverter
 
 func NewLedCubeConverter(dataLength int) CubeConverter {
 	switch dataLength {
-	case 8192:
-		if ledConverter8192 == nil {
-			ledConverter8192 = &ledCubeConverter{16, 32, 8, 8192}
+	case 16 * 32 * 8 * rgb565plane:
+		if ledConverter16x32x8 == nil {
+			ledConverter16x32x8 = newLedCubeConverter(16, 32, 8) // data len 8,192
 		}
-		return ledConverter8192
-	case 22500:
-		if ledConverter22500 == nil {
-			ledConverter22500 = &ledCubeConverter{15, 50, 15, 22500}
+		return ledConverter16x32x8
+	case 15 * 50 * 15 * rgb565plane:
+		if ledConverter15x50x15 == nil {
+			ledConverter15x50x15 = newLedCubeConverter(15, 50, 15) // data len 22,500
 		}
-		return ledConverter22500
+		return ledConverter15x50x15
+	case 30 * 100 * 30 * rgb565plane:
+		if ledConverter30x100x30 == nil {
+			ledConverter30x100x30 = newLedCubeConverter(30, 100, 30) // data len 180,000
+		}
+		return ledConverter30x100x30
 	default:
 		return nil
 	}
 }
 
-func (c *ledCubeConverter) GetCubeWidth() int {
-	return c.width
+func (c *cubeConverterImpl) GetCubeWidth() int {
+	return c.Width
 }
 
-func (c *ledCubeConverter) GetCubeHeight() int {
-	return c.height
+func (c *cubeConverterImpl) GetCubeHeight() int {
+	return c.Height
 
 }
-func (c *ledCubeConverter) GetCubeDepth() int {
-	return c.depth
+func (c *cubeConverterImpl) GetCubeDepth() int {
+	return c.Depth
 }
 
-func (c *ledCubeConverter) ConvertToSudare(cube []byte) []byte {
+func (c *cubeConverterImpl) ConvertToSudare(cube []byte) []byte {
 
-	if len(cube) != c.datalen {
+	if len(cube) != c.DataLen {
 		log.Warn("Invalid datalength ", len(cube))
 		return nil
 	}
 
 	sudareBuf := make([]byte, LedCylinderRadius*LedCylinderHeight*LedCylinderCount*2)
 
-	offsetCubeX := float64(c.width) / 2.0
-	offsetCubeZ := float64(c.depth) / 2.0
+	offsetCubeX := float64(c.Width) / 2.0
+	offsetCubeZ := float64(c.Depth) / 2.0
 
 	cylinderUnitDegree := (2 * math.Pi) / LedCylinderCount
 
-	xscale := float64(c.width) / float64(LedCylinderDiameter)
-	yscale := float64(c.height) / float64(LedCylinderHeight)
+	xscale := float64(c.Width) / float64(LedCylinderDiameter)
+	yscale := float64(c.Height) / float64(LedCylinderHeight)
 
 	util.ConcurrentEnum(0, LedCylinderCount, func(cylinder int) {
 		sin := math.Sin(cylinderUnitDegree * float64(cylinder))
@@ -87,13 +103,13 @@ func (c *ledCubeConverter) ConvertToSudare(cube []byte) []byte {
 			cubex := int(math.Round(offsetCubeX + cos*float64(r)*xscale))
 			cubez := int(math.Round(offsetCubeZ + sin*float64(r)*xscale))
 			for y := 0; y < LedCylinderHeight; y++ {
-				cubey := (c.height - 1) - int(math.Round(float64(y)*yscale))
+				cubey := (c.Height - 1) - int(math.Round(float64(y)*yscale))
 
 				if cubex >= 0 && cubey >= 0 && cubez >= 0 &&
-					cubex < c.width && cubez < c.depth && cubey < c.height {
+					cubex < c.Width && cubez < c.Depth && cubey < c.Height {
 					idxS := ((LedCylinderRadius*cylinder+r)*LedCylinderHeight + y) * 2
 
-					idxC := (cubez + cubey*c.depth + cubex*c.height*c.depth) * 2
+					idxC := (cubez + cubey*c.Depth + cubex*c.Height*c.Depth) * 2
 					sudareBuf[idxS] = cube[idxC]
 					sudareBuf[idxS+1] = cube[idxC+1]
 				}
